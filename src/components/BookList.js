feb922e88622370812042c2d5dabdfc4d41f575c
Allison Schulman (she/her) <68669789+sneakin-allie@users.noreply.github.com>
2022-03-08 13:39:58 -0500

    First commit

---
+++ src/components/BookList.js
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { BookContext } from "../contexts/BookContext";
+import { ThemeContext } from "../contexts/ThemeContext";
+
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div className="book-list" style={{ background: theme.bg, color: theme.syntax}}>
+      <ul>
+        {books.map(book => {
+          return(
+            <li key={book.id} style={{ background: theme.ui }}>{book.title}</li>
+          )
+        })}
+      </ul>
+    </div>
+  );
+}
+ 
+export default BookList;
